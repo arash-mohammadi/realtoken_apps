16d7e3723d149a52a6407f36c4a3f383d36e556f
Byackee <vincent.fresnel@gmail.com>
2025-02-11 20:00:38 +0100

    V1.7.0

--- lib/structure/home_page.dart
+++ lib/structure/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:realtokens/managers/data_manager.dart';
-import 'package:realtokens/structure/Agenda.dart';
+import 'package:realtokens/structure/agenda.dart';
 import 'package:realtokens/utils/ui_utils.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/intl.dart';
